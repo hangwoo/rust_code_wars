d9145de83c1c37eab2d630b302f9d06823b11198
bran <bran@watcha.com>
2021-02-04 19:52:27 +0900

    Fix: string_ends_with

--- string_ends_with/src/main.rs
+++ string_ends_with/src/main.rs
@@ -1,12 +1,30 @@
-fn solution(word: &str, ending: &str) -> bool {
-    word.chars().rev().take(ending.len()).collect::<String>() == ending
+#[cfg(test)]
+mod tests {
+    use crate::solution;
+
+    #[test]
+    fn returns_expected() {
+        assert_eq!(true, solution("abc", "c"));
+        assert_eq!(false, solution("strawberry", "banana"));
+        assert_eq!(false, solution("banana", "strawberry"));
+    }
 }
 
-fn returns_expected() {
-    assert_eq!(true, solution("abc", "c"));
-    assert_eq!(false, solution("strawberry", "banana"));
+pub fn solution(word: &str, ending: &str) -> bool {
+    let start: usize = if word.len() >= ending.len() {
+        word.len() - ending.len()
+    } else {
+        0
+    };
+
+    if start > 0 {
+        return &word[start..] == ending;
+    } else if start == 0 {
+        return word == ending;
+    }
+
+    false
 }
 
 fn main() {
-    returns_expected();
 }
