478dc0e0a91851cf85bfbd31e66b989c21b41532
bran <bran@watcha.com>
2021-02-08 20:39:09 +0900

    Update: backspace_in_string

--- backspace_in_string/src/main.rs
+++ backspace_in_string/src/main.rs
@@ -9,6 +9,14 @@ mod tests {
         assert_eq!(clean_string("#######"), "");
         assert_eq!(clean_string(""), "");
     }
+
+    #[test]
+    fn test2() {
+        assert_eq!(clean_string2("abc#d##c"), "ac");
+        assert_eq!(clean_string2("abc####d##c#"), "");
+        assert_eq!(clean_string2("#######"), "");
+        assert_eq!(clean_string2(""), "");
+    }
 }
 
 // Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
@@ -16,8 +24,34 @@ mod tests {
 // Your task is to process a string with "#" symbols.
 
 fn clean_string(s: &str) -> String {
+    let mut chars: Vec<char> = vec![];
     // your code here
-    String::new()
+    for c in s.chars() {
+        match c {
+            '#' => {
+                chars.pop();
+            },
+            _ => {
+                chars.push(c);
+            }
+        }
+    }
+    chars.iter().collect::<String>()
+}
+
+fn clean_string2(s: &str) -> String {
+    let mut result = String::new();
+    s.chars().for_each(|x| {
+        match x {
+            '#' => {
+                result.pop();
+            },
+            _ => {
+                result.push(x);
+            }
+        }
+    });
+    result
 }
 
 fn main() {
